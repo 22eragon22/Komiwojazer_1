7f798b03af070e73b9f9d643117050d8d7dbe575
22eragon22 <maciejstawiarz1@gmail.com>
2021-11-24 19:57:23 +0100

    Add files via upload

---
+++ ATSP1/cmake-build-debug/CMakeFiles/ATSP1.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Your_/Desktop/ATSP1/main.cpp" "C:/Users/Your_/Desktop/ATSP1/cmake-build-debug/CMakeFiles/ATSP1.dir/main.cpp.obj"
+  "C:/Users/Your_/Desktop/ATSP1/pugixml.cpp" "C:/Users/Your_/Desktop/ATSP1/cmake-build-debug/CMakeFiles/ATSP1.dir/pugixml.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ATSP1/cmake-build-debug/CMakeFiles/ATSP1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "ATSP1.exe"
+  "ATSP1.exe.manifest"
+  "ATSP1.pdb"
+  "CMakeFiles/ATSP1.dir/main.cpp.obj"
+  "CMakeFiles/ATSP1.dir/pugixml.cpp.obj"
+  "libATSP1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ATSP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
